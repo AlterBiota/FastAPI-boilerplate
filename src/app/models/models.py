0a7554377161717e527416253db53287a3f0b999
Vivek Patel <vivek.patel@alterbiota.com>
2024-08-26 13:53:51 -0300

    migration working

---
+++ src/app/models/models.py
@@ -0,0 +1,4 @@
+from src.app.models.post import Post
+from src.app.models.rate_limit import RateLimit
+from src.app.models.tier import Tier
+from src.app.models.user import User
--- src/migrations/env.py
+++ src/migrations/env.py
@@ -7,6 +7,7 @@ from app.core.db.database import Base
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
+from app.models import models
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
